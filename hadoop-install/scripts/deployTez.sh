be48b1f55918b1843b1d80f516554486db8ede49
Hitesh Shah <hitesh@apache.org>
2014-05-01 11:11:08 -0700

    Add configs for hadoop install

--- hadoop-install/scripts/deployTez.sh
+++ hadoop-install/scripts/deployTez.sh
@@ -9,7 +9,7 @@ fi
 cp -f ${tezDir}/tez-dist/target/tez-${tez_version}.tar.gz .
 rm -rf ./tez-${tez_version}
 tar -zxf tez-${tez_version}.tar.gz
-${HADOOP_COMMON_HOME}/bin/hadoop dfs -rm -R /mrx;
-${HADOOP_COMMON_HOME}/bin/hadoop dfs -mkdir /mrx;
-${HADOOP_COMMON_HOME}/bin/hadoop dfs -put ./tez-${tez_version} /mrx/
+${HADOOP_COMMON_HOME}/bin/hadoop dfs -rm -R /tez;
+${HADOOP_COMMON_HOME}/bin/hadoop dfs -mkdir /tez;
+${HADOOP_COMMON_HOME}/bin/hadoop dfs -put ./tez-${tez_version} /tez/
 
