9bd13db0c2f78d871a48124714775cb1eafa23d2
Hitesh Shah <hiteshs@users.noreply.github.com>
2016-04-13 21:02:47 -0700

    Start timeline server

--- hadoop-install/scripts/yarn-start.sh
+++ hadoop-install/scripts/yarn-start.sh
@@ -4,5 +4,5 @@ export HADOOP_ROOT_LOGGER="DEBUG,RFA"
 export YARN_ROOT_LOGGER="DEBUG,RFA"
 $HADOOP_YARN_HOME/sbin/yarn-daemon.sh start resourcemanager
 $HADOOP_YARN_HOME/sbin/yarn-daemon.sh start nodemanager
-$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start historyserver
+$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start timelineserver
 $HADOOP_MAPRED_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
